2198c18656981d6e2fa57c376dc46a7881775206
Ben Wilhelm <ben.wilhelm@nimblemoose.com>
2023-05-01 13:13:20 -0500

    Simple Functions

---
+++ Functions/simple_functions.js
@@ -0,0 +1,18 @@
+function favoriteThings() {
+  console.log(`My name is Ben Wilhelm`);
+  console.log(`My favorite movie is The Martian`);
+  console.log(`My (current) favorite musician is LCD Soundsystem`);
+}
+
+function whyImHere() {
+  console.log("I'm here because I want to teach others to code");
+}
+
+function favoritePlace() {
+  console.log("I love to visit my grandparents' vacation house on Lake Erie");
+  console.log("because it's quiet and peaceful");
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
