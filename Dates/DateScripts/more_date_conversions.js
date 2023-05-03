9ab388c05e46f09a5b39ff3a4186e35d9632c822
Ben Wilhelm <ben.wilhelm@nimblemoose.com>
2023-05-03 09:30:37 -0500

    some demos

--- Dates/DateScripts/more_date_conversions.js
+++ Dates/DateScripts/more_date_conversions.js
@@ -1,6 +1,6 @@
 const myBirthday = new Date('1981-06-24T00:14:00-0400');
-const momsBirthday = new Date('1948-12-17T12:00:00');
-const dadsBirthday = new Date('1948-04-04T12:00:00');
+const momsBirthday = new Date('12/17/1948');
+const dadsBirthday = new Date('April 4, 1948');
 
 console.log(myBirthday.toLocaleString());
 console.log(momsBirthday.toLocaleString());
---
+++ demo3.js
@@ -0,0 +1,7 @@
+let ms = Date.parse('May 3, 2023');
+
+const dateObj = new Date(ms);
+
+console.log('ms', ms);
+console.log('dateObj', dateObj);
+console.log('dateObj.getTime()', dateObj.getTime());
